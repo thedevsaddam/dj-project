fc5398a184124dede94f1e19eaafd5943c5e634a
Saddam H <thedevsaddam@gmail.com>
2016-08-13 03:40:03 +0600

    template added with list of posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+from posts.models import Post
+
+
+def posts_home(request):
+    posts = Post.objects.all()
+    contex = {
+        "posts" : posts
+    }
+    return render(request, 'posts.html', contex)
