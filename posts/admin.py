12c7be78c8438438a775db7aac794063227f3a4e
Saddam H <thedevsaddam@gmail.com>
2016-08-13 03:12:58 +0600

    post custom model admin

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from posts.models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+
+    search_fields = ['title', 'content']
+    list_display = ['__unicode__', 'timestamp', 'updated']
+
+    class Meta:
+        model = Post
+
+
+
+admin.site.register(Post, PostModelAdmin)
